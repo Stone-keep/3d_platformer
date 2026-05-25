9ded212ac0bd4d3bb10dd6930db5e5ae073180f3
Stone-keep <radek12345@gmail.com>
2026-05-25 22:58:40 +0200

    Improve player movement/jumping/gravity

--- scenes/camera_controller.gd
+++ scenes/camera_controller.gd
@@ -1,10 +1,13 @@
 extends Node3D
 
+@onready var yaw_pivot: Node3D = $CameraYawPivot
+@onready var pitch_pivot: Node3D = $CameraYawPivot/CameraPitchPivot
+
 var mouse_sensitivity := 0.004
 var joystick_horizontal_sensitivity := 2.5
 var joystick_vertical_sensitivity := 1.5
-var min_limit_x := -1.5
-var max_limit_x := 0
+var min_pitch := -1.5
+var max_pitch := 0
 
 func _process(delta: float) -> void:
 	var joystick_dir := Input.get_vector("pan_left", "pan_right", "pan_up", "pan_down")
@@ -16,6 +19,6 @@ func _input(event: InputEvent) -> void:
 
 func rotate_from_vector(v: Vector2):
 	if v.length() > 0:
-		rotation.y -= v.x
-		rotation.x -= v.y
-		rotation.x = clamp(rotation.x, min_limit_x, max_limit_x)
+		yaw_pivot.rotation.y -= v.x
+		pitch_pivot.rotation.x -= v.y
+		pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x, min_pitch, max_pitch)
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,36 +1,63 @@
 extends CharacterBody3D
 
-@onready var camera: Camera3D = $CameraController/SpringArm3D/Camera3D
+@onready var camera_yaw_pivot: Node3D = $CameraController/CameraYawPivot
 @onready var model: Node3D = $Model
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
+# Movement
+var speed := 5.0
+var acceleration := 5.0
+var friction := 8.0
 var direction: Vector3
 
+# Jumping
+@export var jump_height: float = 2.5
+@export var jump_time_to_peak: float = 0.4
+@export var jump_time_to_descent: float = 0.3
+
+var jump_velocity: float
+var jump_gravity: float
+var fall_gravity: float
+
+func _ready() -> void:
+	setup_jump_values()
 
 func _physics_process(delta: float) -> void:
-	get_input(delta)
-	move_and_slide()
+	get_input()
+	move(delta)
+	apply_gravity(delta)
 	animate(delta)
+	move_and_slide()
 
-
-func get_input(delta) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
+func get_input() -> void:
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	direction = (camera.global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	direction = (camera_yaw_pivot.global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		jump()
 
+func move(delta):
+	var vel_3d = Vector3(velocity.x, 0, velocity.z)
+	if direction:
+		vel_3d += direction * speed * delta * acceleration
+		vel_3d = vel_3d.limit_length(speed)
+		velocity.x = vel_3d.x
+		velocity.z = vel_3d.z
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed * friction * delta)
+		velocity.z = move_toward(velocity.z, 0, speed * friction * delta)
+
+func setup_jump_values():
+	jump_velocity = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+	jump_gravity = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+	fall_gravity = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
+
+func jump():
+	velocity.y = -jump_velocity
+
+func apply_gravity(delta) -> void:
+	if not is_on_floor():
+		var gravity := jump_gravity if velocity.y > 0 else fall_gravity
+		velocity.y -= gravity * delta
 	
 func animate(delta) -> void:
 	if direction:
