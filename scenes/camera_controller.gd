3cb77e939ce970a42dd2f0027e9ba7ca199e62d2
Stone-keep <radek12345@gmail.com>
2026-05-25 19:53:06 +0200

    Add joystick camera rotation

--- scenes/camera_controller.gd
+++ scenes/camera_controller.gd
@@ -1,14 +1,18 @@
 extends Node3D
 
 @onready var camera := $Camera3D
-var mouse_acceleration := 0.005
+var mouse_sensitivity := 0.004
+var joystick_sensitivity := 2.0
 var min_limit_x := -0.8
 var max_limit_x := -0.2
 
+func _process(delta: float) -> void:
+	var joystick_dir := Input.get_vector("pan_left", "pan_right", "pan_up", "pan_down")
+	rotate_from_vector(joystick_dir * joystick_sensitivity * delta)
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		print(event)
-		rotate_from_vector(event.relative * mouse_acceleration)
+		rotate_from_vector(event.relative * mouse_sensitivity)
 
 func rotate_from_vector(v: Vector2):
 	if v.length() > 0:
